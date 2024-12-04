efa7974b21bcd97679c4543573aa3a635f293160
Rohil Hatalkar <rhatalkar@ramp.com>
2024-12-04 20:21:16 +0530

    Add procfile for heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 import requests
 from enum import Enum
+import os
 
 
 class Position(Enum):
@@ -256,4 +257,5 @@ def get_player_stats(player_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
