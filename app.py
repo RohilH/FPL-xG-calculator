b54b67c43908d7530912e0f6203951d553f24c06
Rohil Hatalkar <rhatalkar@ramp.com>
2024-12-05 16:20:28 +0530

    Fix build?

--- app.py
+++ app.py
@@ -255,10 +255,9 @@ def get_player(player_id):
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    if path and os.path.exists(app.static_folder + "/" + path):
+    if path and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, "index.html")
+    return send_from_directory(app.static_folder, "index.html")
 
 
 if __name__ == "__main__":
